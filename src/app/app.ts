69ef0433465fc5f940ed429985eed61daf6fd445
Ayla Pessoa <aylamargarida21@gmail.com>
2026-07-16 19:27:28 -0300

    feat(products): importar a classe do modelo de produto no componente produto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router'; //remove a importação de RouterOutlet, pois não é necessario no momento;
-import {Produto} from './components/produto/produto';//importando a classe do produto do arquivo produto.ts para produto/produto
-import{ListaProdutos} from './components/lista-produtos/lista-produtos';//importando a classe do produto do arquivo produto.ts para produto/produto
+//import {Produto} from './components/produto/produto';//importando a classe do produto do arquivo produto.ts para produto/produto
+import{ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';//importando a classe do produto do arquivo produto.ts para produto/produto
 @Component({
   selector: 'app-root',
   imports: [ListaProdutos],
